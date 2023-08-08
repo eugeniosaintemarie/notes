04e810abadd095cae5031ea7f9148b18a5b11023
eugeniosaintemarie <eugenio.saintemarie@gmail.com>
2023-08-07 22:18:03 -0300

    20230807

--- js/snackbar.js
+++ js/snackbar.js
@@ -1,21 +1,5 @@
-/**
- * SnackBar.js
- * 
- * This small component is borrowed from 
- * https://codepen.io/wibblymat/pen/avAjq
- */
-
-
 var createSnackbar = (function () {
-  // Any snackbar that is already shown
   var previous = null;
-
-  /*
-  <div class="paper-snackbar">
-    <button class="action">Dismiss</button>
-    This is a longer message that won't fit on one line. It is, inevitably, quite a boring thing. Hopefully it is still useful.
-  </div>
-  */
 
   return function (config) {
     var message = config.message,
@@ -57,12 +41,9 @@ var createSnackbar = (function () {
         }
       }
     }.bind(snackbar));
-
-
-
+    
     previous = snackbar;
     document.body.appendChild(snackbar);
-    // In order for the animations to trigger, I have to force the original style to be computed, and then change it.
     getComputedStyle(snackbar).bottom;
     snackbar.style.bottom = '0px';
     snackbar.style.opacity = 1;
