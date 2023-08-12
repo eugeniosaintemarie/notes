da166370ebc2607a6816ba17c456941331b66636
eugeniosaintemarie <eugenio.saintemarie@gmail.com>
2023-08-11 22:06:07 -0300

    less comments

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,6 +1,4 @@
 module.exports = function (grunt) {
-
-    // Project configuration.
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
         uglify: {
@@ -61,13 +59,10 @@ module.exports = function (grunt) {
         },
     });
 
-    // Load the plugins.
     grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-contrib-less');
     grunt.loadNpmTasks('grunt-banner');
     grunt.loadNpmTasks('grunt-contrib-watch');
-
-    // Default task(s).
     grunt.registerTask('default', ['uglify', 'less', 'usebanner']);
 
 };
